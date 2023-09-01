16bc94e6ca219e0d52382aaa629e070a59fca2ea
Igor Santos <iggor.jsantos@gmail.com>
2023-08-31 21:33:01 -0300

    feat(listagem de pokemons):atribuindo resposta da api para um array

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,9 +1,13 @@
 const offset = 0;
 const limit = 10
 const url = `https://pokeapi.co/api/v2/pokemon?offset=${offset}&limit=${limit}`
+let pokemonList = []
 
 fetch(url)
     .then((response) =>  response.json())
-    .then((jsonBody) =>  console.log(jsonBody))
+    .then((jsonBody) => {
+        pokemonList = jsonBody.results
+        console.log(pokemonList)
+    })
     .catch((error) =>    console.error(error))
     .finally(() =>       console.log("Concluído"))
