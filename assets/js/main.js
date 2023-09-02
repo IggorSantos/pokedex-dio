60c22aa6a7631fbb8752beefa406951a57b56dc3
Igor Santos <iggor.jsantos@gmail.com>
2023-09-02 14:04:04 -0300

    feat(listagem de pokemons): incluindo pokemons da api no HTML

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,13 +1,33 @@
 const offset = 0;
 const limit = 10
 const url = `https://pokeapi.co/api/v2/pokemon?offset=${offset}&limit=${limit}`
-let pokemonList = []
+
+function convertPokemonToHTML(pokemon){
+    return `
+            <li class="pokemon">
+                <span class="number">#001</span>
+                <span class="name">${pokemon.name}</span>
+
+                <div class="detail">
+                    <ol class="types">
+                        <li class="type">gross</li>
+                        <li class="type">poison</li>
+                    </ol>
+                    <img src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/35.png" alt="${pokemon.name}">
+                </div>            
+            </li> 
+    `
+}
+
+const pokemonList = document.getElementById('pokemonList')
 
 fetch(url)
     .then((response) =>  response.json())
-    .then((jsonBody) => {
-        pokemonList = jsonBody.results
-        console.log(pokemonList)
-    })
+    .then((jsonBody) => jsonBody.results)
+    .then((pokemons) => {
+        for(let i = 0; i < pokemons.length; i++){
+            const pokemon = pokemons[i]
+            pokemonList.innerHTML += convertPokemonToHTML(pokemon)
+        }
+    })     
     .catch((error) =>    console.error(error))
-    .finally(() =>       console.log("Concluído"))
